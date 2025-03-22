47d7df4326b47b9ed5b8aeeb035a765afc24ea30
flowers <miketresford@gmail.com>
2025-03-22 11:03:54 +0500

    update js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function redirectToWhatsApp() {
-    const phoneNumber = "+92 3047315455"; 
+    const phoneNumber = "923047315455"; 
     const message = "I love you.. ❤️";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
